924db7ed4af83f8dfefbe5c390fd08dc1d4cca86
Mohamad Safa <mohsafa3@gmail.com>
2023-07-25 16:48:03 +0300

    Done with Task 3

---
+++ task_3/script.js
@@ -0,0 +1,11 @@
+const name1 = 50;
+var name2 = 40;
+var canpass = true;  
+if (canpass) 
+    if (name1 >= name2){
+    console.log("Hello");}
+else
+    {console.log("Bye");}
+else {
+    console.log("Try again")
+}
