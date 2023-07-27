0b67b77f2b9a0f1a223272b20a9031fd60f16a11
Mohamad Safa <mohsafa3@gmail.com>
2023-07-27 16:41:43 +0300

    Done with Task 7

---
+++ task_7/script.js
@@ -0,0 +1,13 @@
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+for (let i = 0; i < numbers.length; i++) {
+
+if (numbers[i] % 3 === 0) {
+    console.log("Divi 3", numbers [i]);
+}
+else if (numbers [i] % 5 ===0) {
+    console.log("Divi 5", numbers [i]);
+}
+else if (numbers[i] % 3, 5 === 0, numbers [i]);
+    console.log("Divi 3&5")
+}
