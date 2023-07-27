7359b083fb2467f9cde4bfc29d21600798f54ff6
Mohamad Safa <mohsafa3@gmail.com>
2023-07-27 16:07:09 +0300

    Done with Task 9

---
+++ task_9/script.js
@@ -0,0 +1,45 @@
+function add (num1, num2);{
+   const x = num1 + num2;
+   console.log (x);
+   return x;
+}
+const result = add (88,10);
+console.log(result);
+
+function substract (num1, num2);{
+    const x = num1 - num2;
+    console.log (x);
+    return x;
+}
+
+function multiply (num1,num2); {
+    const x = num1 * num2;
+    console.log (x);
+    return x;
+
+}
+
+function divide (num1, num2); {
+    if (num2 !==0) {
+    const x = num1 / num2;
+    return x;
+} else {
+    return 'Unable to divide';
+}
+}
+
+function myCalculator (operation, x, y){
+    if (operation === '+'){
+        console.log(add(x, y));
+    } else if (operation === '-') {
+        console.log(substract(x, y));
+    } else if (operation === '*') {
+        console.log(multiply(x, y));    
+    } else if (operation === '/') {
+        console.log(divide (x, y));       
+    } else {
+        console.log('Invalid operation');
+    }
+}
+
+myCalculator ('/', 100, 4);
