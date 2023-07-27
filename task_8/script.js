eb964ea90ffaa195bb422997be7890459459539b
Mohamad Safa <mohsafa3@gmail.com>
2023-07-27 17:12:53 +0300

    Done with Task 8

---
+++ task_8/script.js
@@ -0,0 +1,13 @@
+let mynumbers = [1,2,3,4,5,6,7,8,9,10]
+console.log(mynumbers);
+
+count=0;
+while (count > 8){
+    console.log(count);
+    count = count + 1;
+}
+
+do {
+    count = count + 1;
+    console.log(count);
+} while (count > 8);
