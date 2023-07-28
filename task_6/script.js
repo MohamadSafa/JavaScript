dc10b3b995bc87fc17ceaccb7b6970fd3d5107d1
Mohamad Safa <mohsafa3@gmail.com>
2023-07-28 10:44:22 +0300

    Done with Task 6

--- task_6/script.js
+++ task_6/script.js
@@ -1,5 +1,14 @@
-var button = 5;
+const buttons = document.querySelectorAll('.buttons button');
+const submit = document.getElementById('submit');
+let selected = '';
+submit.addEventListener('click', function () {
+    console.log('test');
+    alert('selected');
 
-function showAlert() {
-    var myText = "This can be whatever text you like!";
-    alert (myText); }
+});
+console.log(buttons);
+buttons.forEach(function (button) {
+    button.addEventListener('click', function () {
+        selected = button.textContent;
+    });
+});
